240bd9b497c10e862b09cd47a60e49f09b26aeba
vauxhall <zero.griffin@gmail.com>
2019-03-06 14:47:37 +0100

    Use default icons

--- Mac/Document.swift
+++ Mac/Document.swift
@@ -13,16 +13,9 @@ class Document: NSControl {
         layer!.cornerRadius = 6
         
         let image = NSImageView()
-        image.image = NSImage(named: {
-            switch $0 {
-            case is meta.Directory: return "folder"
-            case is meta.Image: return "image"
-            case is meta.Pdf: return "pdf"
-            default: return "editable"
-            }
-        } (document))
+        image.image = NSWorkspace.shared.icon(forFile: document.url.path)
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.imageScaling = .scaleNone
+        image.imageScaling = .scaleProportionallyDown
         addSubview(image)
         
         let label = Label(document.name, font: .systemFont(ofSize: 12, weight: .light))
@@ -31,13 +24,13 @@ class Document: NSControl {
         addSubview(label)
         self.label = label
         
-        image.widthAnchor.constraint(equalToConstant: 54).isActive = true
-        image.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        image.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        image.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        image.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        image.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
+        image.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        image.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
         
         label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        label.leftAnchor.constraint(equalTo: image.rightAnchor, constant: -14).isActive = true
+        label.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 2).isActive = true
         label.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
         
         heightAnchor.constraint(equalToConstant: 38).isActive = true

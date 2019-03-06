0c6d80fc9841427cb4e0bf803fbfd7e748861c6d
vauxhall <zero.griffin@gmail.com>
2019-03-06 11:24:42 +0100

    Allow different folders to be opened

--- Library/Folder.swift
+++ Library/Folder.swift
@@ -11,13 +11,15 @@ public class Folder {
     }
     
     public func documents(_ user: User, result: @escaping(([Document]) -> Void)) {
+        user.bookmark.first?.0 == nil ? result([]) : documents(user.bookmark.first!.0, result: result)
+    }
+    
+    public func documents(_ url: URL, result: @escaping(([Document]) -> Void)) {
         DispatchQueue.global(qos: .background).async { [weak self] in
             let documents = {
-                $0 == nil ? nil : {
-                    $0 == nil ? nil : self?.load($0!)
-                } (try? FileManager.default.contentsOfDirectory(at: $0!, includingPropertiesForKeys: []))
-            } (user.bookmark.first?.0) ?? []
-            DispatchQueue.main.async { result(documents) }
+                $0 == nil ? nil : self?.load($0!)
+            } (try? FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: []))
+            DispatchQueue.main.async { result(documents ?? []) }
         }
     }
     

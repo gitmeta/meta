599b0666c65234f05bc3b740827c76e8cf256525
vauxhall <zero.griffin@gmail.com>
2019-03-14 08:22:07 +0100

    Remove async from message

--- Shell/Message.swift
+++ Shell/Message.swift
@@ -2,34 +2,29 @@ import Foundation
 
 class Message {
     class func send(_ string: String, location: URL, response: @escaping((String) -> Void)) {
-        DispatchQueue.global(qos: .background).async {
-            let process = Process()
-            let pipe = Pipe()
-            process.arguments = string.components(separatedBy: " ")
-            process.standardOutput = pipe
-            process.standardError = pipe
-            if #available(OSX 10.13, *) {
-                process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-                process.currentDirectoryURL = location
-                do {
-                    try process.run()
-                } catch {
-                    response(error.localizedDescription)
-                    return
-                }
-            } else {
-                process.launchPath = "/usr/bin/env"
-                process.currentDirectoryPath = location.path
-                process.launch()
+        let process = Process()
+        let pipe = Pipe()
+        process.arguments = string.components(separatedBy: " ")
+        process.standardOutput = pipe
+        process.standardError = pipe
+        if #available(OSX 10.13, *) {
+            process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+            process.currentDirectoryURL = location
+            do {
+                try process.run()
+            } catch {
+                response(error.localizedDescription)
+                return
             }
-            process.waitUntilExit()
-            let answer = {
-                $0.last == "\n" ? String($0.dropLast()) : $0
-            } (String(decoding: pipe.fileHandleForReading.readDataToEndOfFile(), as: UTF8.self))
-            DispatchQueue.main.async {
-                response(answer)
-            }
+        } else {
+            process.launchPath = "/usr/bin/env"
+            process.currentDirectoryPath = location.path
+            process.launch()
         }
+        process.waitUntilExit()
+        response({
+            $0.last == "\n" ? String($0.dropLast()) : $0
+        } (String(decoding: pipe.fileHandleForReading.readDataToEndOfFile(), as: UTF8.self)))
     }
     
     private init() { }

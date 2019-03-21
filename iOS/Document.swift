3b48dce2e16c57039b07eef7e8461f3b49e2cf3d
vauxhall <zero.griffin@gmail.com>
2019-03-21 23:11:51 +0000

    Expand and collapse lists

--- iOS/Document.swift
+++ iOS/Document.swift
@@ -2,38 +2,50 @@ import meta
 import UIKit
 
 class Document: UIControl {
+    weak var parent: Document?
+    weak var top: NSLayoutConstraint? { didSet { oldValue?.isActive = false; top?.isActive = true } }
     let document: meta.Document
+    let indent: CGFloat
     override var isHighlighted: Bool { didSet { update() } }
     override var isSelected: Bool { didSet { update() } }
     private weak var label: UILabel!
+    private weak var image: UIImageView!
     
-    init(_ document: meta.Document) {
+    init(_ document: meta.Document, indent: CGFloat) {
         self.document = document
+        self.indent = indent
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.clipsToBounds = true
-        image.contentMode = .scaleAspectFit
-        image.image = UIDocumentInteractionController(url: document.url).icons.last!
         addSubview(image)
+        self.image = image
         
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .light(14)
         label.text = document.name
+        label.textColor = .white
         addSubview(label)
         self.label = label
         
-        image.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
-        image.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
+        image.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        image.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
         image.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        image.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
+        image.leftAnchor.constraint(equalTo: leftAnchor, constant: 10 + (indent * 40)).isActive = true
         
         label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         label.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 10).isActive = true
         label.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
+        
+        if document is meta.Directory {
+            image.contentMode = .center
+        } else {
+            image.contentMode = .scaleAspectFit
+            image.image = UIDocumentInteractionController(url: document.url).icons.last!
+        }
         
         heightAnchor.constraint(equalToConstant: 50).isActive = true
         update()
@@ -42,12 +54,14 @@ class Document: UIControl {
     required init?(coder: NSCoder) { return nil }
     
     func update() {
-        if List.shared.selected === self || isHighlighted || isSelected {
-            backgroundColor = .halo
-            label.textColor = .black
+        if document is meta.Directory {
+            image.image = isSelected ? #imageLiteral(resourceName: "collapse.pdf") : #imageLiteral(resourceName: "expand.pdf")
         } else {
-            backgroundColor = .clear
-            label.textColor = .white
+            if List.shared.selected === self || isHighlighted || isSelected {
+                alpha = 0.3
+            } else {
+                alpha = 1
+            }
         }
     }
     
--- iOS/List.swift
+++ iOS/List.swift
@@ -9,6 +9,7 @@ class List: UIScrollView {
     var close: NSLayoutConstraint!
     let folder = Folder()
     private weak var content: UIView!
+    private weak var contentBottom: NSLayoutConstraint? { didSet { oldValue?.isActive = false; contentBottom?.isActive = true } }
     
     private init() {
         super.init(frame:.zero)
@@ -46,20 +47,8 @@ class List: UIScrollView {
     func update() {
         content.subviews.filter({ $0 is Document }).forEach({ $0.removeFromSuperview() })
         folder.documents(App.shared.user) {
-            var top = self.topAnchor
-            $0.enumerated().forEach {
-                let document = Document($0.1)
-                document.addTarget(self, action: #selector(self.open(_:)), for: .touchUpInside)
-                self.content.addSubview(document)
-                
-                document.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-                document.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-                document.topAnchor.constraint(equalTo: top, constant: $0.0 == 0 ? 70 + App.shared.margin.top : 0).isActive = true
-                top = document.bottomAnchor
-            }
-            if self.topAnchor !== self.topAnchor {
-                self.content.bottomAnchor.constraint(greaterThanOrEqualTo: top, constant: 30).isActive = true
-            }
+            guard let last = self.render($0, origin: self.topAnchor, margin: 80, parent: nil) else { return }
+            self.align(last)
         }
     }
     
@@ -121,13 +110,64 @@ class List: UIScrollView {
         }
     }
     
+    private func expand(_ document: Document) {
+        folder.documents(document.document.url) {
+            let sibling = self.content.subviews.compactMap({ $0 as? Document }).first(where: { $0.top?.secondItem === document })
+            guard let last = self.render($0, origin: document.bottomAnchor, margin: 0, parent: document) else { return }
+            if let sibling = sibling {
+                sibling.top = sibling.topAnchor.constraint(equalTo: last.bottomAnchor)
+            } else {
+                self.align(last)
+            }
+        }
+    }
+    
+    private func collapse(_ document: Document) {
+        if let sibling = content.subviews.compactMap({ $0 as? Document }).filter({ $0.parent !== document }).first(where:
+            { ($0.top?.secondItem as? Document)?.parent === document }) {
+            sibling.top = sibling.topAnchor.constraint(equalTo: document.bottomAnchor)
+        } else {
+            if (bottom?.secondItem as? Document)?.parent === document {
+                align(document)
+            }
+        }
+        content.subviews.compactMap({ $0 as? Document }).filter({ $0.parent === document }).forEach {
+            collapse($0)
+            $0.removeFromSuperview()
+        }
+    }
+    
+    private func render(_ documents: [meta.Document],
+                        origin: NSLayoutYAxisAnchor, margin: CGFloat, parent: Document?) -> Document? {
+        return documents.reduce((nil, origin, margin)) {
+            let document = Document($1, indent: parent == nil ? 0 : parent!.indent + 1)
+            document.parent = parent
+            document.addTarget(self, action: #selector(self.open(_:)), for: .touchUpInside)
+            content.addSubview(document)
+            
+            document.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+            document.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+            document.top = document.topAnchor.constraint(equalTo: $0.1, constant: $0.2)
+            return (document, document.bottomAnchor, 0)
+            }.0
+    }
+    
+    private func align(_ bottom: UIView) {
+        self.contentBottom = content.bottomAnchor.constraint(greaterThanOrEqualTo: bottom.bottomAnchor, constant: 30)
+    }
+    
     @objc private func open(_ item: Document) {
         guard !App.shared.creating else { return }
-        selected = item
-        Bar.shared.document(item.document.name)
-        if let editable = item.document as? Editable {
-            Display.shared.open(editable)
+        if item.document is meta.Directory {
+            item.isSelected.toggle()
+            item.isSelected ? expand(item) : collapse(item)
+        } else {
+            selected = item
+            Bar.shared.document(item.document.name)
+            if let editable = item.document as? Editable {
+                Display.shared.open(editable)
+            }
+            hide()
         }
-        hide()
     }
 }
